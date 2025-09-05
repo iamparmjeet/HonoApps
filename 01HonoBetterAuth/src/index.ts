4d959ea54665b7a38410db4e15728e265f110520
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-05 21:24:39 +0530

    Add->dockercompose for postgres

--- 01HonoBetterAuth/src/index.ts
+++ 01HonoBetterAuth/src/index.ts
@@ -2,8 +2,17 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+//TODO: Do Seperate Routes and Handler
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono from parm!')
 })
+
+function sayHello(message: string) {
+  console.log(message)
+}
+
+sayHello("hi")
+
 
 export default app
