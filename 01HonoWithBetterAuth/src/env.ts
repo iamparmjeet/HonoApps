bd699dd5c4493a3b75050fe42debc20529185cde
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-07 18:15:24 +0530

    "Update=>Env updated for CF values"

--- 01HonoWithBetterAuth/src/env.ts
+++ 01HonoWithBetterAuth/src/env.ts
@@ -15,16 +15,18 @@ const EnvSchema = z.object({
   NODE_ENV: z.string().default("development"),
   PORT: z.coerce.number().default(9999),
   LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"]),
-  DATABASE_URL: z.string().url(),
-  DATABASE_AUTH_TOKEN: z.string().optional(),
+  CF_DB_NAME: z.string(),
+  CF_D1_DB_ID: z.string(),
+  CF_ACCOUNT_ID: z.string(),
+  CF_TOKEN_ID: z.string(),
 }).superRefine((input, ctx) => {
-  if (input.NODE_ENV === "production" && !input.DATABASE_AUTH_TOKEN) {
+  if (input.NODE_ENV === "production" && !input.CF_TOKEN_ID) {
     ctx.addIssue({
       code: z.ZodIssueCode.invalid_type,
       expected: "string",
       received: "undefined",
-      path: ["DATABASE_AUTH_TOKEN"],
-      message: "DATABASE_AUTH_TOKEN is required in production",
+      path: ["CF_TOKEN_ID"],
+      message: "CF_TOKEN_ID is required in production",
     });
   }
 });
