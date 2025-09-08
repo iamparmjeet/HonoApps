e26cbb38cdf73259abba30dcfc13951f80698360
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-08 20:06:59 +0530

    "update=>randomuuid generate"

--- 01HonoWithBetterAuth/src/middlewares/pino-logger.ts
+++ 01HonoWithBetterAuth/src/middlewares/pino-logger.ts
@@ -1,6 +1,6 @@
+import { randomUUID } from "node:crypto";
 import type { Context, MiddlewareHandler } from "hono";
 import type { Env } from "hono-pino";
-
 import { pinoLogger as logger } from "hono-pino";
 import pino from "pino";
 import pretty from "pino-pretty";
@@ -8,12 +8,19 @@ import pretty from "pino-pretty";
 import type { AppBindings } from "@/lib/types";
 
 export function pinoLogger() {
-  return ((c, next) => logger({
-    pino: pino({
-      level: c.env.LOG_LEVEL || "info",
-    }, c.env.NODE_ENV === "production" ? undefined : pretty()),
-    http: {
-      reqId: () => crypto.randomUUID(),
-    },
-  })(c as unknown as Context<Env>, next)) satisfies MiddlewareHandler<AppBindings>;
+  return ((c, next) =>
+    logger({
+      pino: pino(
+        {
+          level: c.env.LOG_LEVEL || "info",
+        },
+        c.env.NODE_ENV === "production" ? undefined : pretty()
+      ),
+      http: {
+        reqId: () => randomUUID(),
+      },
+    })(
+      c as unknown as Context<Env>,
+      next
+    )) satisfies MiddlewareHandler<AppBindings>;
 }
