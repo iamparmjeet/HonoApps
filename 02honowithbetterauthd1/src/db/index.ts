c58e4fb245199a70bda5d1f4cf2f5301f663951f
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-23 23:13:18 +0530

    Update=>fixed environment

--- 02honowithbetterauthd1/src/db/index.ts
+++ 02honowithbetterauthd1/src/db/index.ts
@@ -1,11 +1,12 @@
 import { drizzle } from "drizzle-orm/d1";
 import * as schema from "@/db/schema";
-// import type { Environment } from "@/env";
+import type { Environment } from "@/env";
 
-// export interface ExtendedEnv extends Environment {
-//   CF_DB: D1Database;
-// }
-export function createDB(env: { CF_DB: D1Database }) {
+export interface EnvWithD1 extends Environment {
+  CF_DB: D1Database;
+}
+
+export function createDB(env: EnvWithD1) {
   return drizzle(env.CF_DB, {
     casing: "snake_case",
     schema,
