c1a24613ab6edd72917051e7d5e42ddd6fd0b4df
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-23 21:45:51 +0530

    Update=>c.env error fixed

--- 02honowithbetterauthd1/src/middleware/auth-middleware.ts
+++ 02honowithbetterauthd1/src/middleware/auth-middleware.ts
@@ -1,11 +1,11 @@
 import type { Context, Next } from "hono";
-import { auth } from "@/lib/auth";
+import { createAuth } from "@/lib/auth";
 import type { ExtendedUser } from "@/types/types";
 import { StatusCode, safeError, sendError, unauthorized } from "@/utils";
 
 const withAuth = () => async (c: Context, next: Next) => {
   try {
-    // const auth = createAuth(db);
+    const auth = createAuth(c.env);
     const result = await auth.api.getSession({ headers: c.req.raw.headers });
 
     if (!result || !result.user || !result.session) {
